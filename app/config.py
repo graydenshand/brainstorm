01f672cf0edbaa1ae3ac8b6d5054b20172ddbc97
Grayden Shand <graydenshand@gmail.com>
2020-10-19 17:15:04 -0400

    Set up domain mail server

--- app/config.py
+++ app/config.py
@@ -4,13 +4,12 @@ class Config(object):
 	SECRET_KEY = 'g]fQU<XfE:5"%QkV'
 	DEBUG = False
 	TESTING = False
-	MAIL_SERVER = 'smtp.googlemail.com'
-	MAIL_PORT = 465
-	MAIL_USE_TLS = False
-	MAIL_USE_SSL = True
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = os.environ.get('MAIL_PORT')
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+	MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
 	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
 
 class ProductionConfig(Config):
 	pass
--- app/email.py
+++ app/email.py
@@ -2,6 +2,8 @@ from flask_mail import Message
 from flask import render_template
 from app import mail
 
+sender = ("Timed Brainstorm", "hello@timedbrainstorm.com")
+
 def build_message(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
@@ -9,7 +11,7 @@ def build_message(subject, sender, recipients, text_body, html_body):
     return msg
 
 def send_test_message():
-    msg = build_message('test', ('Grayden', 'graydenshand@gmail.com'), ['shandgp@clarkson.edu'], 'test', "<p>test</p>")
+    msg = build_message('test', sender, ['shandgp@clarkson.edu'], 'test', "<p>test</p>")
     mail.send(msg)
 
 def send_results(session):
@@ -23,6 +25,5 @@ def send_results(session):
             text += '----\n'.join([post for post in session['posts']])
             text += 'Thanks for using Timed Brainstorm!'
             subject = "Your brainstorm results"
-            sender = ("Grayden", "graydenshand@gmail.com")
             msg = build_message(subject, sender, [email], text, html)
             conn.send(msg)
