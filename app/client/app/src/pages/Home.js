269a0569bbc1977eec4cc61e74e5abbac2d1a7cb
Grayden Shand <graydenshand@gmail.com>
2020-10-23 15:26:54 -0400

    Design home page

--- app/client/app/src/pages/Home.js
+++ app/client/app/src/pages/Home.js
@@ -15,8 +15,6 @@ export default class Home extends React.Component {
       durationHours: 1,
       durationMinutes: 0,
       durationError: "",
-      sessionCreated: false,
-      session_id: "",
       email: "",
       emailError: "",
     }
@@ -40,7 +38,7 @@ export default class Home extends React.Component {
         .then(data => {
           this.addEmail(data.session_id)
           .then(response => {
-            this.setState({sessionCreated: true, session_id: data.session_id})
+            window.location.pathname = "/" + data.session_id
           })
           .catch(error => console.log(error))
         })
@@ -133,57 +131,70 @@ export default class Home extends React.Component {
     dateMax = dateMax.toISOString().split('T')[0]
 
     return (
-      <div className="bg-gray-50 min-h-screen px-5 text-green-800">
-        {this.state.sessionCreated ? (<Redirect to={'/' + this.state.session_id} />) : ""}
-        <h1 className="max-w-screen-md mx-auto text-center font-semibold py-10 text-green-500 uppercase leading-none tracking-wide">Timed Brainstorm</h1>
-        <h1 className="text-3xl mb-4 font-black max-w-screen-sm mx-auto">
-          Start a new brainstorming session
-        </h1>
-        <form className="max-w-screen-sm mx-auto" onSubmit={this.handleSubmit}>
-          <div>
-            <label for="titleInput" className="text-lg font-semibold" >Title</label>
-            <input type="text" id="titleInput" value={this.state.title} onChange={this.handleTitleChange} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
-            <p className="text-sm text-red-500">{this.state.titleError}</p>
-          </div>
-          <div className="mt-4">
-            <label for="descriptionInput" className="text-lg font-semibold">Description</label>
-            <ExpandingTextArea id="descriptionInput" value={this.state.description} minHeight="100px" handleChange={this.handleDescriptionChange} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline"></ExpandingTextArea>
-          </div>
-          <div className="mt-4">
-            <label className="text-lg font-semibold">Duration</label>
-            <div className="flex justify-between items-center">
-              <div className="w-full mr-6">
-                <label for="durationDaysInput" className="text-xs font-semibold uppercase text-green-500 tracking-wider" >Days</label>
-                <input type="number" id="durationDaysInput" placeholder="days" onChange={this.handleDurationDaysChange} min="0" max="6" value={this.state.durationDays} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
-              </div>
-              <div className="w-full mx-6">
-              <label for="durationHoursInput" className="text-xs font-semibold uppercase text-green-500 tracking-wider" >Hours</label>
-                <input type="number" id="durationHoursInput" placeholder="hours" onChange={this.handleDurationHoursChange} min="0" max="23" value={this.state.durationHours} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
-              </div>
-              <div className="w-full ml-6">
-                <label for="durationMinutesInput" className="text-xs font-semibold uppercase text-green-500 tracking-wider" >Minutes</label>
-                <input type="number" id="durationMinutesInput" placeholder="minutes" onChange={this.handleDurationMinutesChange} min="0" max="59" value={this.state.durationMinutes} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
-              </div>
+      <div className="bg-gray-800 min-h-screen text-green-800 flex justify-center items-center">
+        <div className="max-w-screen-xl mx-auto md:grid md:grid-cols-3 md:grid-flow-col-dense bg-white rounded">
+          <form className="bg-gray-100 p-5 md:col-start-3 rounded-r py-10 md:py-5" onSubmit={this.handleSubmit}>
+            <h1 className="text-3xl mb-4 text-green-700 font-black leading-none">
+              Start a new <br/>brainstorming session
+            </h1>
+            <div>
+              <label for="titleInput" className="text-lg font-semibold" >Title</label>
+              <input type="text" id="titleInput" value={this.state.title} onChange={this.handleTitleChange} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
+              <p className="text-sm text-red-500">{this.state.titleError}</p>
             </div>
-            <p className="text-sm text-red-500">{this.state.durationError}</p>
-          </div>
-          <div className="mt-4">
-            <div className='flex items-center'>
-              <label for="emailInput" className="text-lg font-semibold ">Email</label>
-              <p className="text-sm text-green-500 hover:text-green-800 ml-2 cursor-pointer">Privacy policy</p>
+            <div className="mt-4">
+              <label for="descriptionInput" className="text-lg font-semibold">Description</label>
+              <ExpandingTextArea id="descriptionInput" value={this.state.description} minHeight="100px" handleChange={this.handleDescriptionChange} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline"></ExpandingTextArea>
             </div>
-            <input type="text" id="emailInput" name="email" value={this.state.email} placeholder="bob@example.com" onChange={this.handleEmailChange} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
-            <p className="text-sm">We'll email you the results</p>
-            <p className="text-sm text-red-500">{this.state.emailError}</p>
+            <div className="mt-4">
+              <label className="text-lg font-semibold">Duration</label>
+              <div className="flex justify-between items-center">
+                <div className="w-full mr-6">
+                  <label for="durationDaysInput" className="text-xs font-semibold uppercase text-green-500 tracking-wider" >Days</label>
+                  <input type="number" id="durationDaysInput" placeholder="days" onChange={this.handleDurationDaysChange} min="0" max="6" value={this.state.durationDays} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
+                </div>
+                <div className="w-full mx-6">
+                <label for="durationHoursInput" className="text-xs font-semibold uppercase text-green-500 tracking-wider" >Hours</label>
+                  <input type="number" id="durationHoursInput" placeholder="hours" onChange={this.handleDurationHoursChange} min="0" max="23" value={this.state.durationHours} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
+                </div>
+                <div className="w-full ml-6">
+                  <label for="durationMinutesInput" className="text-xs font-semibold uppercase text-green-500 tracking-wider" >Minutes</label>
+                  <input type="number" id="durationMinutesInput" placeholder="minutes" onChange={this.handleDurationMinutesChange} min="0" max="59" value={this.state.durationMinutes} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
+                </div>
+              </div>
+              <p className="text-sm text-red-500">{this.state.durationError}</p>
+            </div>
+            <div className="mt-4">
+              <div className='flex items-center'>
+                <label for="emailInput" className="text-lg font-semibold ">Email</label>
+              </div>
+              <input type="text" id="emailInput" name="email" value={this.state.email} placeholder="bob@example.com" onChange={this.handleEmailChange} className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
+              <p className="text-sm">We'll email you the results</p>
+              <p className="text-sm text-red-500">{this.state.emailError}</p>
+            </div>
+            <div className="flex items-center my-5 text-yellow-600 p-2 border border-yellow-500 bg-yellow-50 rounded">
+              <svg class="w-6 h-6 mr-2 flex-shrink-0 flex-grow-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+              <p className="leading-tight font-semibold ">Please note, you cannot change these fields after your session has started.</p>
+            </div>
+            <div className="flex justify-center mt-8">
+              <button className="w-full sm:w-auto bg-green-500 px-8 py-4 mb-10 md:mb-0 text-lg tracking-wide font-bold text-white  hover:bg-green-700 transition-colors duration-400 rounded">Start Session</button>
+            </div>
+          </form>
+          <div className="md:col-span-2 md:col-start-1 bg-green-500 text-green-50 md:grid md:grid-cols-2 p-5 rounded-l">
+            <div className="px-2 pt-10 md:pt-0">
+              <p className="text-xl mb-3 font-semi-bold">Welcome to <strong>Timed Brainstorm,</strong> a free and open source tool for collaboratively generating ideas.</p>
+              <p className="text-xl mb-3 font-semi-bold">You can start a new brainstorming session using the form on this page.</p>
+              <p className="text-xl mb-3 font-semi-bold">Once you've created your session, you can <strong>invite others to the page by sharing the session URL.</strong></p>
+              <p className="text-xl mb-3 font-semi-bold"><strong>Users post anonymously.</strong> This helps generate more ideas faster. However, it also means you must be careful about sharing your session link. Anyone with the link can anonymously contribute to your session.</p>
+              <p className="text-xl mb-3 font-semi-bold">After your session has ended, <strong>we'll send you an email with the results for your records. Then, all of the data from your session is erased&mdash;</strong>we only retain statistics on session such as the number of ideas generated.</p>
+            </div>
+            <div className="px-2">
+              <p className="text-xl mb-3 font-semi-bold">Thanks for visiting the site. If you find it useful, please donate to charity and share it with your friends.</p>
+              <p className="text-xl mb-3 font-semi-bold">You can view the code for this app on <a href="https://github.com/graydenshand/brainstorm" target="_blank" className="underline">GitHub</a>.</p>
+              <p className="text-xl mb-3 font-semi-bold">And you can email me feedback or bug reports at <a href="mailto:hello@timedbrainstorm.com" target="_blank" className="underline">hello@timedbrainstorm.com</a>.</p>
+            </div>
           </div>
-          <div className="flex items-center my-5 text-green-800">
-            <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <p>Please note, you cannot change these fields after your session has started.</p>
-          </div>
-          <div className="flex justify-center mt-8">
-            <button className="w-full sm:w-auto bg-green-500 px-10 py-5 text-xl tracking-wide font-bold text-white hover:bg-green-700 rounded">Start Session</button>
-          </div>
-        </form>
+        </div>
       </div>
     )
   }
--- app/client/app/src/pages/Session.js
+++ app/client/app/src/pages/Session.js
@@ -235,7 +235,7 @@ class Session extends React.Component {
                   <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </div>
               </div>
-              <p className="text-sm leading-5">Add your email below to receive the final results of this brainstorming session. Your email address will be erased from our system after we send you the results. <a href="#" className="text-green-500 hover:text-green-800">Privacy policy</a></p>
+              <p className="text-sm leading-5">Add your email below to receive the final results of this brainstorming session. Your email address will be erased from our system after we send you the results.</p>
               <form onSubmit={this.handleSubmitEmail}>
                 <div className="flex mt-4">
                   <input type='email' value={this.state.emailFormInput} onChange={this.handleEmailFormChange} name='email' placeholder='bob@example.com' className="w-full border border-gray-500 rounded py-1 px-2 focus:border-white  outline-none focus:shadow-outline" />
